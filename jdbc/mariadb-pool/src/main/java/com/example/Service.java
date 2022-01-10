462bbcd4ddc7b0e6fed77ad68c42257610201d7a
alejandro-du <alejandro.d.a@gmail.com>
2022-01-10 21:25:30 +0200

    Move mariadb-pool example

---
+++ jdbc/mariadb-pool/src/main/java/com/example/Service.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+import org.mariadb.jdbc.MariaDbPoolDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * You can configure the database connection in the constructor.
+ * You need an SQL database with the following table:
+ *
+ * <pre>
+ * CREATE TABLE programming_language(
+ *     name VARCHAR(50) NOT NULL UNIQUE,
+ *     Rating INT
+ * );
+ * </pre>
+ *
+ * See the comments in the constructor for more instructions.
+ *
+ */
+public class Service {
+
+    private static Service instance;
+
+    private DataSource dataSource;
+
+    private Service() throws SQLException {
+        // First try with a DataSource without pooling:
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        // That should fail (SQLException: too many connections)
+        // Try now commenting the previous executable line
+        // and using the following DataSource that supports pooling:
+        //MariaDbPoolDataSource dataSource = new MariaDbPoolDataSource();
+        // That should work!
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/jdbc_demo");
+        dataSource.setUser("user");
+        dataSource.setPassword("password");
+        this.dataSource = dataSource;
+    }
+
+    public static synchronized Service getInstance() throws SQLException {
+        return instance == null ? instance = new Service() : instance;
+    }
+
+    public List<String> getAllProgrammingLanguages() throws SQLException {
+        var list = new ArrayList<String>();
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "SELECT name FROM programming_language"
+            )) {
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    list.add(resultSet.getString("name"));
+                }
+            }
+        }
+        return list;
+    }
+
+}
